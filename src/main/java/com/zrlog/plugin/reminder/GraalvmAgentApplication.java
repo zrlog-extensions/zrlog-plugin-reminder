ae0532e3ee1def97347f9bb1ae31ad58140a2b1d
xiaochun <90xchun@gmail.com>
2026-05-28 08:24:13 +0800

    add upx for linux/windows

--- src/main/java/com/zrlog/plugin/reminder/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/reminder/GraalvmAgentApplication.java
@@ -8,14 +8,14 @@ import com.zrlog.plugin.reminder.model.ReminderTask;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class GraalvmAgentApplication {
 
     public static void main(String[] args) throws IOException {
         PluginNativeImageUtils.usedGsonObject();
-        new Gson().toJson(new ReminderTask());
-        new Gson().toJson(new ReminderStore());
+        PluginNativeImageUtils.gsonNativeAgentByClazz(Arrays.asList(ReminderStore.class,ReminderTask.class));
         String basePath = System.getProperty("user.dir").replace("\\target", "").replace("/target", "");
         File file = new File(basePath + "/src/main/resources");
         PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath() + "/", "/");
